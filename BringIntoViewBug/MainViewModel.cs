e9a24f5020fb16acca7df1d65540ed977612de75
DSPAUL <paul.de.smul@gmail.com>
2023-11-03 23:19:26 +0100

    Initial commit

---
+++ BringIntoViewBug/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace BringIntoViewBug
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            Items = new(Enumerable.Range(0, 1000));
+        }
+
+        public ObservableCollection<int> Items { get; set; }
+    }
+}
---
+++ BringIntoViewBug/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace BringIntoViewBug
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainViewModel vm = new();
+            DataContext = vm;
+        }
+    }
+}
